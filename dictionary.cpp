0ab5e76c6441f7b738eb2e74b44ecd9364ab8c8d
Rafael Hernandez <reh119@txstate.edu>
2020-09-10 23:35:50 +0000

    searchcppHW2

--- dictionary.cpp
+++ dictionary.cpp
@@ -3,29 +3,31 @@
 *
 */
 #include <string>
+#include "dictionary.h"
+
 using namespace std;
 
-int MinWordLength(string words[]) {
+int MinWordLength(string words[], int length) {
     // TODO implement this function
     throw "Unsupported Operation";
 }
 
-int MaxWordLength(string words[]) {
+int MaxWordLength(string words[], int length) {
     // TODO implement this function
     throw "Unsupported Operation";
 }
 
-int WordLengthRange(string words[]) {
+int WordLengthRange(string words[], int length) {
     // TODO implement this function
     throw "Unsupported Operation";
 }
 
-int AverageWordLength(string words[]) {
+int AverageWordLength(string words[], int length) {
     // TODO implement this function
     throw "Unsupported Operation";
 }
 
-int MostCommonWordLength(string words[]) {
+int MostCommonWordLength(string words[], int length) {
     // TODO implement this function
     throw "Unsupported Operation";
 }
--- search.cpp
+++ search.cpp
@@ -1,13 +1,63 @@
 /****
-* PSEUDOCODE:
+* PSEUDOCODE: Binary Search
+for loop to go through array 1 by 1 
+if target is found in first position, we're done. Target has been found.
+return position back to main
+
+since its a for loop, it will go through list until target is found. 
+
+*
+*/
+/****
+
+* PSEUDOCODE: LinearSearch
+while first number is less than our last AND found is false
+set middle equal to middle number in our list. 
+
+if the number set to middle is equal to our target, then we are done.
+set found true 
+return mid (position).
+if number is less than middle (cuts last half off)
+change last to middle - 1
+if number is greater than middle 
+change first to middle +1 (cuts first half off)
+
 *
 */
 int LinearSearch(int numbers[], int length, int n) {
-    // TODO implement this function
-   return 1;
+    
+    for (int position = 0; position < length; position++){
+  if (numbers[position]==n){
+    return position; 
+  }
+    }
+   return -1;
 }
 
 int BinarySearch(int numbers[], int length, int n) {
-    // TODO implement this function
-     return 1;
+    
+  int first = 0;
+  int last = length-1;
+  int mid = 0; 
+  bool found = false;
+
+  while (first<=last && !found){
+  mid = (first+last)/2;
+
+  if (n == numbers[mid]){
+   found = true;
+   return mid;
+  }
+  else if (n < numbers [mid]){
+  last = (mid -1);
+  }
+  else 
+  first = (mid +1);
+
+  }
+
+
+
+     return -1;
 }
+
